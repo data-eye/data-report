56db828206dd48d7dd4b92297eeb2f730c8eaf3e
fengshuan <fengshuan@yoozoo.com>
2020-08-29 20:27:59 +0800

    feat: 流量收益模块

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>Top View</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover">
+          <total-sales />
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import TotalSales from "../TotalSales";
 export default {
-  name: "TopView"
+  name: "TopView",
+  components: {
+    TotalSales
+  }
 };
 </script>
 
---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <common-card title="流量收益" :value="salesToday">
+    <template>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span>
+          <span class="emphasis">{{ salesGrowthLastDay }}</span>
+          <div class="increase" />
+        </div>
+        <div class="compare">
+          <span>月同比</span>
+          <span class="emphasis">{{ salesGrowthLastMonth }}</span>
+          <div class="decrease" />
+        </div>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日收益</span>
+      <span class="emphasis">{{ salesLastDay }}</span>
+    </template>
+  </common-card>
+</template>
+
+<script>
+import commonCardMixin from "../../mixins/commonCardMixin";
+
+export default {
+  mixins: [commonCardMixin],
+  data() {
+    return {
+      salesToday: "¥39,040",
+      salesGrowthLastDay: "3.01%",
+      salesGrowthLastMonth: "5.78%",
+      salesLastDay: "¥18,400"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.compare-wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .compare {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    margin-top: 3px;
+    color: #666;
+  }
+}
+</style>
